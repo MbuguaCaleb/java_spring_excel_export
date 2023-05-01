004ef1a7d66d05e0bcde6c25ba4cb8ec057bf948
MbuguaCaleb <mbuguacaleb30@gmail.com>
2023-05-01 11:22:57 +0300

    ssh-keys-test

--- src/main/java/com/codewithcaleb/exporttoexcelfilespringdemo/domain/Address.java
+++ src/main/java/com/codewithcaleb/exporttoexcelfilespringdemo/domain/Address.java
@@ -1,5 +1,8 @@
 package com.codewithcaleb.exporttoexcelfilespringdemo.domain;
 
 public class Address {
-
+    private String country;
+    private String state;
+    private String city;
+    private String address;
 }
